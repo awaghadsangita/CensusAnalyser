535a2c33e607be54dc97f4387ba224ede00665f9
sangita awaghad <sangita.awaghad@gmail.com>
2019-12-13 17:28:30 +0530

    ADDED:testcase given IndiaCensus CSV files but with incorrect delimitor

--- src/test/java/censusanalyser/CensusAnalyserTest.java
+++ src/test/java/censusanalyser/CensusAnalyserTest.java
@@ -11,6 +11,7 @@ public class CensusAnalyserTest {
     private static final String US_CENSUS_CSV_FILE_PATH = "/home/admin1/Downloads/CensusAnalyser/CensusAnalyser/src/test/resources/USCensusData.csv";
     private static final String INCORRECT_INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData1.csv";
     private static final String INCORRECT_INDIA_CENSUS_CSV_FILE_TYPE_PATH = "./src/test/resources/IndiaStateCensusData.json";
+    private static final String INDIA_CENSUS_CSV_WITH_INCORRECT_DELIMITER_FILE_PATH = "/home/admin1/Downloads/CensusAnalyser/CensusAnalyser/src/test/resources/IndiaStateCensusData_incorrect_delimiter.csv";
     @Test
     public void givenIndianCensusCSVFile_ReturnsCorrectRecords() {
         try {
@@ -149,6 +150,17 @@ public class CensusAnalyserTest {
             Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
         }
     }
+
+    @Test
+    public void givenIndianCensusCSVFile_ButIncorrectDelimiter_ShouldThrowsCustomException() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser(CensusAnalyser.Country.INDIA);
+            censusAnalyser.loadCensusData(INDIA_CENSUS_CSV_WITH_INCORRECT_DELIMITER_FILE_PATH, INDIA_STATE_CSV_FILE);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.PROBLEM_WITH_HEADER_FORMAT, e.type);
+        }
+    }
+
     @Test
     public void givenUsCensusData_WithCorrectFile_ShouldReturnCorrectRecords() {
         CensusAnalyser censusAnalyser = new CensusAnalyser(CensusAnalyser.Country.US);
@@ -159,7 +171,7 @@ public class CensusAnalyserTest {
         }
     }
 
-    @Test
+   @Test
     public void givenUsCensusData_WithSortedOnState_ShouldReturnStateStartWithLowestAlphabet() {
         CensusAnalyser censusAnalyser = new CensusAnalyser(CensusAnalyser.Country.US);
         try {
